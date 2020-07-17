9be01a266b52e37297bc74052b4552f5669b128e
subroto-baroi <67515709+subroto-baroi@users.noreply.github.com>
2020-07-17 19:37:31 +0600

    added all edit file

--- assignment.js
+++ assignment.js
@@ -9,6 +9,7 @@ var result = feetToMile(10560);
 console.log(result);
 
 
+
 // woodCalculator
 
 function woodCalculator(chair, table, bed){
@@ -21,16 +22,40 @@ function woodCalculator(chair, table, bed){
 var woodResult = woodCalculator(10, 4, 3);
 console.log(woodResult);
 
+
 // brickCalculator
+
+
+function brickCalculator(floor){
+    console.log(floor);
+    if (floor > 0 && floor <=10){
+        var brick = 15*1000*floor;
+        console.log(brick);
+    }else if (floor > 10 && floor <=20 ){
+        var brick = 12*1000*(floor-10)+150000;
+        console.log(brick);
+    }else if (floor > 20){
+        var brick = 10*1000*(floor -20) + 150000 + 120000;
+        console.log(brick);
+    }
+
+}
+brickCalculator(10);
+brickCalculator(20);
+brickCalculator(30);
+brickCalculator(40);
+
+
+
 
 // tinyFriend
 
 
 function tinyFriend(name){
-    let smallest = name[0];
+    var smallest = name[0];
         if(name.length > 0){
-          for (let i = 0; i <name.length; i++){
-             if(typeof name[i] === 'string' && name[i].length < smallest.length)
+          for (var i = 0; i <name.length; i++){
+             if(typeof name[i] == 'string' && name[i].length < smallest.length)
             {
                  smallest = name[i];
             }
@@ -41,3 +66,5 @@ function tinyFriend(name){
 }
 var output = tinyFriend (['Shisir', 'Ripa', 'Nik', 'Shumaia', 'Babul']);
 console.log(output);
+
+
